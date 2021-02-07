406044026ab68c80b507401327a1d4e2fb95db5b
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-02-07 16:54:41 +0530

    Enable building ZLIB

--- skyhookdm-image/script.sh
+++ skyhookdm-image/script.sh
@@ -9,11 +9,12 @@ cmake -DCMAKE_BUILD_TYPE=Debug \
           -DARROW_CLS=ON \
           -DARROW_PARQUET=ON \
           -DARROW_WITH_SNAPPY=ON \
+          -DARROW_WITH_ZLIB=ON \
           -DARROW_DATASET=ON \
           -DARROW_PYTHON=ON \
           -DARROW_CSV=ON \
           ..
-    
+
 make -j4 install
 
 cp ./debug/libcls_arrow* /usr/lib64/rados-classes/
