09c533991a8b8f9bfe79d5b951b5d64bfb07125d
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-02-09 11:59:14 +0530

    Consolidate C++ and Python API along with CLS into a single image (#6)

--- skyhookdm-image/script.sh
+++ skyhookdm-image/script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-set -ex
+set -eux
 
-cd arrow/cpp
+# c++ api and cls build
+cd /arrow/cpp
 
 mkdir -p debug
 cd debug
@@ -19,3 +20,31 @@ make -j4 install
 
 cp ./debug/libcls_arrow* /usr/lib64/rados-classes/
 cp -r /usr/local/lib64/. /usr/local/lib
+
+# python api build
+cd /arrow/python
+
+pip3 install -r requirements-build.txt -r requirements-test.txt
+pip3 install wheel
+
+export WORKDIR=${WORKDIR:-$HOME}
+export ARROW_HOME=$WORKDIR/dist
+export LD_LIBRARY_PATH=$ARROW_HOME/lib
+export PYARROW_BUILD_TYPE=Debug
+export PYARROW_WITH_DATASET=1
+export PYARROW_WITH_PARQUET=1
+export PYARROW_WITH_RADOS=1
+
+mkdir -p /root/dist/lib
+mkdir -p /root/dist/include
+
+cp -r /usr/local/lib/. /root/dist/lib
+cp -r /usr/local/include/. /root/dist/include
+
+python3 setup.py build_ext --inplace --bundle-arrow-cpp bdist_wheel
+pip3 install dist/*.whl
+
+# check installation
+python3 -c "import pyarrow"
+python3 -c "import pyarrow.dataset"
+python3 -c "import pyarrow.parquet"
