a8af1f57a7809d9bfc8afa48f795a4f1eceec627
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-07-19 11:28:18 +0530

    Update the trigger script

--- build_release_image.sh
+++ build_release_image.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -eux
+set -x
 
 BRANCH=$1
 
 git push -d origin $BRANCH
+git checkout master
 git branch -D $BRANCH
 
 git checkout -b $BRANCH
 git push --set-upstream origin $BRANCH
+git checkout master
