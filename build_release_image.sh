614c5550080078084da0e38379e5bd9ee1057f28
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-07-19 12:56:47 +0530

    bugfix: fix build_release_image.sh script

--- build_release_image.sh
+++ build_release_image.sh
@@ -8,5 +8,8 @@ git checkout master
 git branch -D $BRANCH
 
 git checkout -b $BRANCH
+echo "# Triggering release build" >> .github/workflows/skyhook-benchmark.yml
+git add .github/workflows/skyhook-benchmark.yml
+git commit -m "Trigger release build"
 git push --set-upstream origin $BRANCH
 git checkout master
